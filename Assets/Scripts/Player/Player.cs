b60697e1beb4c40f5ee8e6459875dfda0441b89c
GustavoHGarres <ghgarres@gmail.com>
2025-02-19 14:11:24 -0300

    Acrescentando animações: Attack, Kick, Jump

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -15,13 +15,20 @@ public float _currentSpeed;
 [Header("Animation Player")]
 public string boolRun = "Run";
 public string boolRunSpeed = "RunSpeed";
+public string boolAttack = "Attack";
+public string boolKick = "Kick";
+
+public string boolJump = "Jump";
 
 public Animator animator;
 
 private void Update()
 {
 HandleJump();
+HandleBoolJump();
 HandleMoviment();
+HandleAttack();
+HandleKick();
 }
 
 private void HandleMoviment()
@@ -39,11 +46,11 @@ private void HandleMoviment()
             animator.SetBool(boolRunSpeed, true);
            }
 
-        else if (Input.GetKey(KeyCode.LeftControl))  
+       // else if (Input.GetKey(KeyCode.LeftControl))  
 
-        {
-            animator.SetBool(boolRunSpeed, true);
-        } 
+       // {
+           // animator.SetBool(boolRunSpeed, true);
+       // } 
 
         else
         {
@@ -93,6 +100,46 @@ private void HandleJump()
       myRigidbody.velocity = Vector2.up * forceJump;
      }
 
+}
+
+private void HandleBoolJump()
+{
+      if (Input.GetKeyDown(KeyCode.Space))
+           {
+            animator.SetBool(boolJump, true);
+           }
+ 
+      else
+        {
+             animator.SetBool(boolJump, false);
+        }   
+}
+
+
+private void HandleAttack()
+{
+      if (Input.GetKeyDown(KeyCode.A))
+           {
+            animator.SetBool(boolAttack, true);
+           }
+ 
+      else
+        {
+             animator.SetBool(boolAttack, false);
+        }   
+}
+
+private void HandleKick()
+{
+      if (Input.GetKeyDown(KeyCode.S))
+           {
+            animator.SetBool(boolKick, true);
+           }
+ 
+      else
+        {
+             animator.SetBool(boolKick, false);
+        }   
 }
 
 }
